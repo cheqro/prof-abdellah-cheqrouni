b89921b9ee5803179f17db8670b86ee4174a036b
cheqro <cheqrounikarim@gmail.com>
2025-04-22 09:58:42 +0200

    fc3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {PdfViewerModule} from 'ng2-pdf-viewer';
+import {HttpClient} from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    PdfViewerModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+
+  pdfSrc = 'assets/pdfs/dummy.pdf';
+
   imageUrl="https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
+
+
+  constructor(private http: HttpClient) {}
+
+  downloadPDF() {
+    this.http.get('assets/pdfs/dummy.pdf', { responseType: 'blob' }).subscribe((data: Blob) => {
+      const file = new Blob([data], { type: 'application/pdf' });
+      saveAs(file, 'downloaded-file.pdf');
+    });
+  }
 }
+
