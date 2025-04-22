c27edb5abb80f4075f090f31f5d70d3503c94172
cheqro <cheqrounikarim@gmail.com>
2025-04-22 22:07:09 +0200

    fc4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {Bac01Component} from './lycee/bac01/bac01.component';
+import {Bac02Component} from './lycee/bac02/bac02.component';
+import {TroncCommunScienceComponent} from './lycee/tronc-commun-science/tronc-commun-science.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'bac01', component: Bac01Component },
+  { path: 'bac02', component: Bac02Component },
+  { path: 'tronc-commun-science', component: TroncCommunScienceComponent },
 ];
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 interface MenuItem {
   icon: string;
   label: string;
+  link: string;
   children?: MenuItem[];
   isOpen?: boolean;
 }
@@ -12,7 +14,8 @@ interface MenuItem {
   imports: [
     NgClass,
     NgForOf,
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
@@ -26,24 +29,23 @@ export class SidebarComponent {
       icon: 'fas fa-home',
       label: 'Accueil',
       isOpen: false,
+      link:"//"
     },
     {
       icon: 'fas fa-book',
       label: 'Tronc commun science',
       isOpen: false,
+      link:"/tronc-commun-science"
     },
     {
       icon: 'fa-solid fa-folder-open',
       label: 'Bac',
       isOpen: false,
+      link: "/home",
       children: [
-        { icon: 'fas fa-user', label: '01 Baccalauréat' },
-        { icon: 'fas fa-lock', label: '02 Baccalauréat' },
+        { icon: 'fas fa-user', label: '01 Baccalauréat', link: '/bac01' },
+        { icon: 'fas fa-lock', label: '02 Baccalauréat', link: '/bac02' },
       ]
-    },
-    {
-      icon: 'fas fa-envelope',
-      label: 'Messages'
     }
   ];
 
@@ -52,9 +54,21 @@ export class SidebarComponent {
   }
 
   toggleMenuItem(item: MenuItem) {
+    if(item.children && item.children.length > 0) {
+      console.log(item.children);
+    }else {
+      this.sidebarToggle.emit();
+      console.log(item);
+
+    }
     // Only toggle if sidebar is not collapsed and item has children
     if (!this.isSidebarCollapsed && item.children) {
       item.isOpen = !item.isOpen;
     }
+  }
+
+  closeSideNav() {
+    console.log('closeSideNav');
+    this.sidebarToggle.emit();
   }
 }
